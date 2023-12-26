9838f4bf7b73d9a9709886bc4ca92830fe8d704c
Sagar Zambare <you@example.com>
2023-12-26 15:39:13 +0530

    First testNG framework commit

---
+++ src/test/java/com/qc/testng/test/LoginTest.java
@@ -0,0 +1,47 @@
+package com.qc.testng.test;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseIntegration {
+	
+	private String tName;
+
+	@BeforeMethod
+	public void locateElement() {
+		email = driver.findElement(By.id("email"));
+		email.clear();
+		pass = driver.findElement(By.id("password"));
+		pass.clear();
+		signin = driver.findElement(By.id("submit"));	
+	}
+	@Test(dataProvider = "loginData")
+	public void doLogin(String testName, String uName, String uPass) {
+		tName = testName;
+		email.sendKeys(uName);
+		pass.sendKeys(uPass);
+		signin.click();
+	}
+	@AfterMethod
+	public void doAssert() throws InterruptedException {
+		Thread.sleep(3000);
+		String actResult = driver.getTitle();
+		String expResult = "";
+		if(tName.endsWith("Both are valid")) {
+			expResult = "Queue Codes | Dashboard";
+			doLogout();
+		}else {
+			expResult = "Queue Codes | Log in";
+		}
+		Assert.assertEquals(actResult, expResult);
+	}
+	
+	public void doLogout() {
+		logout = driver.findElement(By.id("hlogout"));
+		logout.click();
+	}
+
+}
